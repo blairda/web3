82d1b82e2fffd27060bd3213c17b594f3102acdf
blairda <bda34@drexel.edu>
2020-07-09 16:06:55 -0400

    Progress on tinySlider customized

--- dev/js/tinySlider.js
+++ dev/js/tinySlider.js
@@ -5,5 +5,41 @@ tns(
     container: '.my-slider',
     items: 1,
     slideBy: 'page',
-    autoplay: false
+    autoplay: false,
+    gutter: 5,
+    edgePadding: 20,
+    navPosition: "bottom",
+    controls: false,
+    responsive: {
+        768: {
+            edgePadding: 100,
+            gutter: 10
+        },
+        1025: {
+            edgePadding: 100,
+            gutter: 20
+        },
+        1440: {
+            edgePadding: 100,
+            gutter: 30
+        }
+    }
 });
+
+// tns({
+//     container: '.my-slider',
+//     items: 1,
+//     responsive: {
+//       768: {
+//         edgePadding: 20,
+//         gutter: 20,
+//         items: 2
+//       },
+//       1025: {
+//         gutter: 30
+//       },
+//       1440: {
+//         items: 3
+//       }
+//     }
+//   });

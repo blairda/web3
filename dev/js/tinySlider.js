1254f406724c3f6d436d9384da48a0b084b56702
blairda <bda34@drexel.edu>
2020-07-21 22:37:33 -0400

    Fix tinySlider and burger/nav clicks

--- dev/js/tinySlider.js
+++ dev/js/tinySlider.js
@@ -1,32 +1,36 @@
 // console.log("tinySlider here");
-// if ($(this).hasClass("my-slider")) {
-tns(
-    {
-    container: '.my-slider',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    loop: true,
-    gutter: 10,
-    edgePadding: 40,
-    navPosition: "bottom",
-    controls: false,
-    mouseDrag: true,
-    arrowKeys: true,
-    speed: 500,
-    responsive: {
-        768: {
-            edgePadding: 80,
-            gutter: 20
-        },
-        1025: {
-            edgePadding: 120,
-            gutter: 30
-        },
-        1440: {
-            edgePadding: 160,
-            gutter: 40
-        }
+
+$(document).ready(function () {
+
+    console.log($(".slides").hasClass("my-slider"));
+    if ($(".slides").hasClass("my-slider")) {
+        tns({
+            container: '.my-slider',
+            items: 1,
+            slideBy: 'page',
+            autoplay: false,
+            loop: true,
+            gutter: 10,
+            edgePadding: 40,
+            navPosition: "bottom",
+            controls: false,
+            mouseDrag: true,
+            arrowKeys: true,
+            speed: 500,
+            responsive: {
+                768: {
+                    edgePadding: 80,
+                    gutter: 20
+                },
+                1025: {
+                    edgePadding: 120,
+                    gutter: 30
+                },
+                1440: {
+                    edgePadding: 160,
+                    gutter: 40
+                }
+            }
+        });
     }
 });
-// }

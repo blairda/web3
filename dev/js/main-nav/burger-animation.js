c8bb4369f45d484248b00b079cc37340306b599c
blairda <bda34@drexel.edu>
2020-07-23 18:41:37 -0400

    Add overflow to punctuation

--- dev/js/main-nav/burger-animation.js
+++ dev/js/main-nav/burger-animation.js
@@ -5,7 +5,7 @@ var burgerAnimationSpeed = 0.33;
 // reset transformOrigin point for each bar
 gsap.set(".bars",{transformOrigin:"center"});
 
-// reference to timeline variable above, animated object, animate property/ies *only 1 objectid/class per action
+// reference to timeline variable above, animated object, animate property/ies *only 1 objectid/class per action, color is orange
 // Ex: variablename.action("#objectid",{property,property}); optional add after },"timelinestampname")
 burgerAnimationTimeline.to("#middle-bar",{duration:burgerAnimationSpeed, alpha:0, fill: "#E18A37"}, "burgerStart")
                         .to("#top-bar",{duration:burgerAnimationSpeed, rotation:45, y:11, fill: "#E18A37"}, "burgerStart")
@@ -26,7 +26,7 @@ function animateBurger(){
 
 }
 
-// function to revert back to original yellow color when reversing from X to burger
+// function to revert back to original yellow color when reversing from X to burger, color is yellow
 function resetBurgerBarsColor(){
     gsap.to(".bars",{duration:burgerAnimationSpeed, fill: "#D2AF36"});
 }

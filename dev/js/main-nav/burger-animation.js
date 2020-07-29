b96fb9ffabdf990152e645cb1efadc8e0ac08f17
blairda <bda34@drexel.edu>
2020-07-28 22:23:56 -0400

    Finish burger to down arrow

--- dev/js/main-nav/burger-animation.js
+++ dev/js/main-nav/burger-animation.js
@@ -1,17 +1,24 @@
 // defining variables
 var burgerAnimationTimeline = gsap.timeline({paused:true});
-// var burgerAnimationSpeed = 0.33;
+var burgerAnimationSpeed = 0.33;
 
 // reset transformOrigin point for each bar
 gsap.set(".bars",{transformOrigin:"center"});
 
-// // reference to timeline variable above, animated object, animate property/ies *only 1 objectid/class per action, color is orange
-// // Ex: variablename.action("#objectid",{property,property}); optional add after },"timelinestampname")
+// reference to timeline variable above, animated object, animate property/ies *only 1 objectid/class per action, color is orange
+// Ex: variablename.action("#objectid",{property,property}); optional add after },"timelinestampname")
 // burgerAnimationTimeline.to("#middle-bar",{duration:burgerAnimationSpeed, alpha:0, fill: "#E18A37"}, "burgerStart")
 //                         .to("#top-bar",{duration:burgerAnimationSpeed, rotation:45, y:11, fill: "#E18A37"}, "burgerStart")
 //                         .to("#bottom-bar",{duration:burgerAnimationSpeed, rotation:-45, y:-15, fill: "#E18A37", onReverseComplete:resetBurgerBarsColor}, "burgerStart");
 
 // burgerAnimationTimeline.to("#burger");
+
+burgerAnimationTimeline.to("#top-bar", {duration:burgerAnimationSpeed, rotation:45}, "burgerToX")
+                        .to("#bottom-bar", {duration:burgerAnimationSpeed, rotation:-45}, "burgerToX")
+                        .to("#middle-bar", {duration:burgerAnimationSpeed, alpha:0}, "burgerToX")
+                        .to("#left-arrow", {duration:burgerAnimationSpeed, alpha:0, rotation:0}, "burgerToX")
+                        .to("#right-arrow", {duration:burgerAnimationSpeed, alpha:0, rotation:0}, "burgerToX")
+
 
 
 function animateBurger(){

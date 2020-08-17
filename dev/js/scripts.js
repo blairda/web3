1ed6e467df22e2d73bea9b1cf6ab3490edbddd05
blairda <bda34@drexel.edu>
2020-08-16 22:22:04 -0400

    Add page title timeline animations

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -9,6 +9,9 @@
 // @codekit-prepend "main-nav/nav-scroll.js";
 // @codekit-prepend "main-nav/main-nav-click.js";
 
-$( document ).ready(function() {
-    console.log( "ready" );
-});
+// $( document ).ready(function() {
+//     console.log( "ready" );
+// });
+
+gsap.from(".title", {duration:1, alpha:0, yPercent: -50}, "title")
+gsap.from(".site-title", {duration:1, alpha:0, xPercent: -50}, "title");
